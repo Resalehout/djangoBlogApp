bfcc742cb18436bcbba551612ebca820674fb1ee
Mayor Onyebueke <mayoronyebueke@gmail.com>
2021-05-14 23:06:00 +0100

    Update settings.py

--- djangoBlog/settings.py
+++ djangoBlog/settings.py
@@ -83,11 +83,8 @@ WSGI_APPLICATION = 'djangoBlog.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": env.dj_db_url("DATABASE_URL")
     }
-}
 
 
 # Password validation
