9a285d2aa21496998856410e0f706f47ab0e67ff
Mayor Onyebueke <mayoronyebueke@gmail.com>
2021-05-14 22:01:03 +0100

    Update admin.py

--- blogApp/admin.py
+++ blogApp/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'comment', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('author', 'comment')
+    actions = ['approve_comments']
+    
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Comment)
